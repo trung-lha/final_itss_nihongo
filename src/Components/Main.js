bd26b4964858e2fa28453bedb0ee0a309e626c99
trung-lha <trung.lha100899@gmail.com>
2021-09-09 08:46:14 +0700

    complete

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React , {useState} from "react";
 import useStorage from "../hooks/useStorage";
 
 function Main () {
+    const [input, setInput] = useState('');
+    const {arrayName, indexName, searchItem} = useStorage()
+
+    const handleChange = (e) => {
+        console.log(e.target.value);
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = () => {
+        searchItem(input);
+    }
     return (
         <div>
-            Hello
+            <p>
+                学生一覧：[{arrayName.map((element, index) =>
+                index === arrayName.length - 1 ? (<span>{element}</span>) : (<span>{element},&nbsp;</span>)
+            )}]
+            </p>
+            <p>
+                検索名前：
+                <input value={input} onChange={handleChange}/>
+                <button type="submit" onClick={() => handleSubmit()}>確認</button>
+            </p>
+            <p>
+                検索名前: {input}
+            </p>
+            <p>
+                位置：{indexName　=== 0 ? 'NaN' : indexName}
+            </p>
         </div>
     );
 }
--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function useStorage () {
-    return [];
+    const arrayName = ['Huyen', 'Hoa', 'Hung', 'Long'];
+    const [indexName, setIndexName] = useState(null);
+
+    const searchItem = (item) => {
+        const index = arrayName.findIndex(i => i === item)
+        setIndexName(index + 1);
+    }
+
+    return {arrayName, searchItem, indexName};
 }
 
 export default useStorage;
